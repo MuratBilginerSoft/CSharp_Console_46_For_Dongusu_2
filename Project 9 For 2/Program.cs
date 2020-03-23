91675a9114f2a8591887be6136f133743e00c04b
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 04:17:14 +0300

    Initial commit

---
+++ Project 9 For 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_9_For_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen 10 sayı içinde pozitif ve tek olan sayıların çarpımını yazdıran For döngüsü ile yapılmış Console örneği:
+
+            //int carpim = 1;
+            //int Sayi;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Bir Sayı Giriniz:");
+            //    Sayi = Convert.ToInt32(Console.ReadLine());
+
+            //    if (Sayi > 0 && Sayi % 2 != 0)
+            //    {
+            //        carpim = carpim * Sayi;
+            //    }
+
+            //}
+
+            int N = 10;
+
+            int j = 0;
+            j = j + 1;
+            j++;
+
+            int i = 0;
+
+            for (; ;)
+            {
+
+            }
+
+            Console.WriteLine("Carpım={0}",carpim);
+
+            for (char Harf = 'a'; Harf <= 'z'; Harf++)
+            {
+                Console.WriteLine(Harf);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_46_For_Dongusu_2
+ 
